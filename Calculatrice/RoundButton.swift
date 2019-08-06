addb36a6c5c4b7514fd13bb54880167ed04016fc
Paul Colombier <paul.colombier@epsi.fr>
2019-08-06 10:50:09 +0200

    functionality + UI

---
+++ Calculatrice/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  Calculatrice
+//
+//  Created by Paul Colombier on 06/08/2019.
+//  Copyright © 2019 Paul Colombier. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
---
+++ Calculatrice/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  Calculatrice
+//
+//  Created by Paul Colombier on 19/07/2019.
+//  Copyright © 2019 Paul Colombier. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var _shouldResetCurrentNumber : Bool = true
+    var _operationType : Character = " "
+    var _previousNumber : Double = 0
+    var _currentNumber : Double = 0 {
+        didSet {
+            ui_currentNumberLabel.text = "\(_currentNumber)"
+        }
+    }
+    
+    @IBOutlet weak var ui_currentNumberLabel: UILabel!
+    
+    func performWaitingCalculous() {
+        let result : Double
+        if _operationType == "+" {
+            result = _previousNumber + _currentNumber
+        } else if _operationType == "-" {
+            result = _previousNumber - _currentNumber
+        } else if _operationType == "*" {
+            result = _previousNumber * _currentNumber
+        } else if _operationType == "/" {
+            result = _previousNumber / _currentNumber
+        } else {
+            result = _currentNumber
+        }
+        _previousNumber = result
+        _shouldResetCurrentNumber = true
+        ui_currentNumberLabel.text = "\(result)"
+    }
+    
+    @IBAction func divide() {
+        if _shouldResetCurrentNumber == false {
+            performWaitingCalculous()
+        }
+        _operationType = "/"
+    }
+    @IBAction func multiply() {
+        if _shouldResetCurrentNumber == false {
+            performWaitingCalculous()
+        }
+        _operationType = "*"
+    }
+    @IBAction func substract() {
+        if _shouldResetCurrentNumber == false {
+            performWaitingCalculous()
+        }
+        _operationType = "-"
+    }
+    @IBAction func add() {
+        if _shouldResetCurrentNumber == false {
+            performWaitingCalculous()
+        }
+        _operationType = "+"
+    }
+    @IBAction func displayResults() {
+        performWaitingCalculous()
+    }
+    @IBAction func resetValue() {
+        _previousNumber = 0
+        _currentNumber = 0
+        _operationType = " "
+    }
+    @IBAction func changeSign() {
+        _currentNumber = _currentNumber * -1
+    }
+    @IBAction func applyPercent() {
+        _currentNumber = _currentNumber / 100
+    }
+    
+    @IBAction func digitButtonTouched(_ sender: UIButton) {
+        if _shouldResetCurrentNumber == true {
+            _currentNumber = Double(sender.tag)
+            _shouldResetCurrentNumber = false
+        } else {
+            _currentNumber = _currentNumber * 10 + Double(sender.tag)
+        }
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
